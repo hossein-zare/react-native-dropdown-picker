f789425e0a1ccff65344ce820cc9f181b4965ceb
Hossein Zare <zarehossein.hz1997@gmail.com>
2020-08-25 16:26:41 +0430

    Remove duplicate properties.

--- index.d.ts
+++ index.d.ts
@@ -22,7 +22,6 @@ declare module 'react-native-dropdown-picker' {
     arrowColor?: string;
     arrowSize?: number;
     showArrow?: boolean;
-    scrollViewProps?: Object;
     customArrowUp?: (size: number, color: string) => JSX.Element;
     customArrowDown?: (size: number, color: string) => JSX.Element;
     customTickIcon?: () => JSX.Element;
