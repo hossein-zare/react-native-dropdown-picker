5e95483ee7274c80a4a9de8222f51f06121ebf19
timor <timor.s@committed.co.il>
2020-10-26 15:32:54 +0200

    add seperator between items ===>  renderSeperator
    renderSeperator is a function witch return a component between an items,
    For example I would like a to have a line between the items,
    So i will use it like that:
    <DropDownPicker
          ....
          renderSeperator={() => <View style={{ backgroundColor: 'red', height: 2 }}></View>}
        />

--- index.d.ts
+++ index.d.ts
@@ -45,6 +45,7 @@ declare module 'react-native-dropdown-picker' {
     onClose?: () => void;
     onChangeItem?: (item: any, index: number) => void;
     onChangeList?: (items: any, callback: () => void) => void;
+    renderSeperator?: () => JSX.Element;
   };
   const DropDownPicker: ComponentType<DropDownPicker>;
   export default DropDownPicker;
