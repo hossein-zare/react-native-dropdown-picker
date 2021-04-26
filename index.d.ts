04853ba88d32e9455e8145f2ee5df4cdc6c5d2fb
Hossein Zare <zarehossein.hz1997@gmail.com>
2021-04-27 02:49:05 +0430

    Component type definitions.

--- index.d.ts
+++ index.d.ts
@@ -6,121 +6,197 @@ declare module "react-native-dropdown-picker" {
       TextStyle,
       ViewStyle,
       ScrollViewProps,
+      ModalProps,
+      FlatListProps,
       ViewProps,
       TextInputProps,
+      LayoutChangeEvent
     } from "react-native";
   
+    export type ValueType = string | number;
+
     export type ItemType = {
       label?: any;
-      value?: any;
+      value?: ValueType;
       icon?: () => void;
       parent?: any;
       selectable?: boolean;
       disabled?: boolean;
     };
+
+    export type ModeType  = "SIMPLE" | "BADGE";
+
+    export interface ModeInterface {
+      DEFAULT: string;
+      SIMPLE: string;
+      BADGE: string;
+    };
+
+    export type ListModeType = "FLATLIST" | "SCROLLVIEW" | "MODAL";
+
+    export interface ListModeInterface {
+      DEFAULT: string;
+      FLATLIST: string;
+      SCROLLVIEW: string;
+      MODAL: string;
+    };
+
+    export interface SchemaInterface {
+      label: string;
+      value: string;
+      icon: string;
+      parent: string;
+      selectable: string;
+      disabled: string;
+    };
+
+    export type LanguageType = "EN" | "AR" | "FA" | "TR";
+    
+    export interface TranslationInterface {
+      PLACEHOLDER: string;
+      SEARCH_PLACEHOLDER: string;
+      SELECTED_ITEMS_COUNT_TEXT: string;
+      NOTHING_TO_SHOW: string;
+    };
+
+    export interface RenderBadgeItemPropsInterface {
+      label: string;
+      value: ValueType;
+      IconComponent: () => JSX;
+      textStyle: StyleProp<TextStyle>;
+      badgeStyle: StyleProp<ViewStyle>;
+      badgeTextStyle: StyleProp<TextStyle>;
+      badgeDotStyle: StyleProp<ViewStyle>;
+      getBadgeColor: (value: string) => string;
+      getBadgeDotColor: (value: string) => string;
+      showBadgeDot: boolean;
+      onPress: (value: ValueType) => void;
+    };
+
+    export interface ActivityIndicatorComponentPropsInterface {
+      size: number;
+      color: string;
+    };
+
+    export interface ListEmptyComponentPropsInterface {
+      listMessageContainer: StyleProp<ViewStyle>;
+      listMessageTextStyle: StyleProp<TextStyle>;
+      ActivityIndicatorComponent: (props: ActivityIndicatorComponentPropsInterface) => JSX
+      loading: boolean;
+      message: string;
+    };
   
     export type DropDownPickerProps = {
       items: ItemType[];
       value?: string | number;
+      open: boolean;
       placeholder?: string;
-      maxHeight?: number;
-      style?: StyleProp<ViewStyle>;
-      dropDownStyle?: StyleProp<ViewStyle>;
-      containerStyle?: StyleProp<ViewStyle>;
-      itemStyle?: StyleProp<ViewStyle>;
-      labelStyle?: StyleProp<TextStyle>;
-      selectedLabelStyle?: StyleProp<TextStyle>;
-      placeholderStyle?: StyleProp<TextStyle>;
-      activeItemStyle?: StyleProp<ViewStyle>;
-      activeLabelStyle?: StyleProp<TextStyle>;
-      arrowStyle?: StyleProp<ViewStyle>;
-      arrowColor?: string;
-      arrowSize?: number;
-      showArrow?: boolean;
-      customArrowUp?: (size: number, color: string) => JSX.Element;
-      customArrowDown?: (size: number, color: string) => JSX.Element;
-      customTickIcon?: () => JSX.Element;
-      zIndex?: number;
-      zIndexInverse?: number;
-      disabled?: boolean;
-      isVisible?: boolean;
-      autoScrollToDefaultValue?: boolean;
-      multiple?: boolean;
-      multipleText?: string;
-      min?: number;
-      max?: number;
-      searchable?: boolean;
-      searchablePlaceholder?: string;
-      searchablePlaceholderTextColor?: string;
-      searchableStyle?: StyleProp<TextStyle>;
-      searchableError?: () => JSX.Element;
-      onSearch?: (text: string) => void;
-      selectedLabelLength?: number;
-      labelLength?: number;
+      closeAfterSelecting?: boolean;
       labelProps?: TextProps;
+      disabled?: boolean;
+      disabledStyle?: StyleProp<ViewStyle>;
+      placeholderStyle?: StyleProp<TextStyle>;
+      containerStyle?: StyleProp<ViewStyle>;
+      style?: StyleProp<ViewStyle>;
+      textStyle?: StyleProp<TextStyle>;
+      labelStyle?: StyleProp<TextStyle>;
+      arrowIconStyle?: StyleProp<ViewStyle>;
+      tickIconStyle?: StyleProp<ViewStyle>;
+      closeIconStyle?: StyleProp<ViewStyle>;
+      badgeStyle?: StyleProp<ViewStyle>;
+      badgeTextStyle?: StyleProp<TextStyle>;
+      badgeDotStyle?: StyleProp<ViewStyle>;
+      iconContainerStyle?: StyleProp<ViewStyle>;
+      searchContainerStyle?: StyleProp<ViewStyle>;
+      searchTextInputStyle?: StyleProp<TextStyle>;
+      searchPlaceholderTextColor?: string;
+      dropDownContainerStyle?: StyleProp<ViewStyle>;
+      modalContentContainerStyle?: StyleProp<ViewStyle>;
+      closeIconContainerStyle?: StyleProp<ViewStyle>;
+      tickIconContainerStyle?: StyleProp<ViewStyle>;
+      listItemContainerStyle?: StyleProp<ViewStyle>;
+      listItemLabelStyle?: StyleProp<TextStyle>;
+      listChildContainerStyle?: StyleProp<ViewStyle>;
+      listChildLabelStyle?: StyleProp<TextStyle>;
+      listParentContainerStyle?: StyleProp<ViewStyle>;
+      listParentLabelStyle?: StyleProp<TextStyle>;
+      selectedItemContainerStyle?: StyleProp<ViewStyle>;
+      listItemContainerStyle?: StyleProp<ViewStyle>;
+      selectedItemLabelStyle?: StyleProp<TextStyle>;
+      disabledItemContainerStyle?: StyleProp<ViewStyle>;
+      disabledItemLabelStyle?: StyleProp<TextStyle>;
+      customItemContainerStyle?: StyleProp<ViewStyle>;
+      customItemLabelStyle?: StyleProp<TextStyle>;
+      listMessageContainerStyle?: StyleProp<ViewStyle>;
+      listMessageTextStyle?: StyleProp<TextStyle>;
+      itemSeparatorStyle?: StyleProp<ViewStyle>;
+      badgeSeparatorStyle?: StyleProp<ViewStyle>;
+      listMode?: ListModeType;
+      categorySelectable?: boolean;
+      searchable?: boolean;
+      searchPlaceholder?: string;
+      schema?: SchemaInterface;
+      language?: LanguageType;
+      translation?: TranslationInterface;
+      multiple?: boolean;
+      mode?: ModeType;
+      key?: string;
+      maxHeight?: number;
+      renderBadgeItem?: (props: RenderBadgeItemPropsInterface) => JSX,
+      renderListItem?: (props: object) => JSX,
+      itemSeparator?: boolean;
+      bottomOffset?: number;
+      badgeColors?: string[] | string;
+      badgeDotColors?: string[] | string;
+      showArrowIcons?: boolean;
+      showBadgeDot?: boolean;
+      ArrowUpComponent?: (props: {style: StyleProp<ViewStyle>}) => JSX;
+      ArrowDownComponent?: (props: {style: StyleProp<ViewStyle>}) => JSX;
+      TickIconComponent?: (props: {style: StyleProp<ViewStyle>}) => JSX;
+      CloseIconComponent?: (props: {style: StyleProp<ViewStyle>}) => JSX;
+      ListEmptyComponent?: (props: ListEmptyComponentPropsInterface) => JSX;
+      ActivityIndicatorComponent?: (props: ActivityIndicatorComponentPropsInterface) => JSX;
+      activityIndicatorSize?: number;
+      activityIndicatorColor?: string;
+      props?: ViewProps;
+      modalProps?: ModalProps;
+      flatListProps?: FlatListProps;
       scrollViewProps?: ScrollViewProps;
       searchTextInputProps?: TextInputProps;
+      loading?: boolean;
+      min?: number;
+      max?: number;
+      addCustomItem?: boolean;
+      setOpen: () => void;
+      setItems: () => void;
+      disableBorderRadius?: boolean;
       containerProps?: ViewProps;
-      globalTextStyle?: StyleProp<TextStyle>;
-      childrenContainerStyle?: StyleProp<ViewStyle>;
-      noTopRadius?: Boolean;
-      noBottomRadius?: Boolean;
-      bottomOffset?: number;
-      controller?: (instance: DropDownPickerInstanceType) => void;
+      onLayout?: (e: LayoutChangeEvent) => void;
+      onPressStart?: (open: boolean) => void;
+      onPressEnd?: (open: boolean) => void;
       onOpen?: () => void;
       onClose?: () => void;
-      onChangeItem?: (item: any, index: number) => void;
-      onChangeItemMultiple?: (item: any) => void;
-      onChangeList?: (items: any, callback: () => void) => void;
-      renderSeperator?: () => JSX.Element;
+      onChangeValue: (item: ValueType | ValueType[]) => void;
+      onChangeSearchText?: (text: string) => void;
+      zIndex?: number;
     };
 
     type GetSelectedItemOutputType = ItemType | undefined | null;
     type GetSelectedItemsOutputType = ItemType[] | undefined;
 
-    export interface TranslationInterface {
-      PLACEHOLDER: string,
-      SEARCH_PLACEHOLDER: string,
-      SELECTED_ITEMS_COUNT_TEXT: string,
-      NOTHING_TO_SHOW: string
-    }
-
     interface DropDownPickerInterface {
-      MODE: {
-        DEFAULT: string,
-        SIMPLE: string,
-        BADGE: string
-      },
-      LIST_MODE: {
-        DEFAULT: string,
-        FLATLIST: string,
-        SCROLLVIEW: string,
-        MODAL: string
-      },
-      SCHEMA: {
-        label: string,
-        value: string,
-        icon: string,
-        parent: string,
-        selectable: string,
-        disabled: string
-      },
-      LANGUAGE: {
-        DEFAULT: string,
-        FALLBACK: string,
-        ENGLISH: string,
-        ARABIC: string,
-        FARSI: string,
-        TURKISH: string
-      },
+      MODE: ModeInterface,
+      LIST_MODE: ListModeInterface,
+      SCHEMA: SchemaInterface,
+      LANGUAGE: LanguageInterface,
       HELPER: {
-        GET_SELECTED_ITEM: (items: ItemType[], value: string | number | null, key?: string) => GetSelectedItemOutputType,
-        GET_SELECTED_ITEMS: (items: ItemType[], values: string[] | number[] | null, key?: string) => GetSelectedItemsOutputType,
+        GET_SELECTED_ITEM: (items: ItemType[], value: string | number | null, key?: string) => GetSelectedItemOutputType;
+        GET_SELECTED_ITEMS: (items: ItemType[], values: string[] | number[] | null, key?: string) => GetSelectedItemsOutputType;
       },
-      setMode: (mode: string) => void,
-      setListMode: (mode: string) => void,
-      setLanguage: (language: string) => void,
-      addTranslation: (language: string, translation: TranslationInterface) => void
+      setMode: (mode: string) => void;
+      setListMode: (mode: string) => void;
+      setLanguage: (language: string) => void;
+      addTranslation: (language: string, translation: TranslationInterface) => void;
     }
 
     const DropDownPicker: ComponentType<DropDownPickerProps> & DropDownPickerInterface;
