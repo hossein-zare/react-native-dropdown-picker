cef79828b3a617da0b40094c8c3fae789d4150e7
Tom Hardern <thomasaeh1@gmail.com>
2021-05-05 00:38:21 +0100

    removed unused "thene" field in RenderBadgeItemPropsInterface, since it doesn't appear (separately from THEME) in RenderBadgeItem.js

--- index.d.ts
+++ index.d.ts
@@ -72,7 +72,6 @@ declare module "react-native-dropdown-picker" {
       showBadgeDot: boolean;
       onPress: (value: ValueType) => void;
       rtl: boolean;
-      thene: string;
       THEME: object;
     }
 
