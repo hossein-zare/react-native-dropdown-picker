5302299e4d4135fc266b040efd24155411732ad8
Hossein Zare <56504893+hossein-zare@users.noreply.github.com>
2022-03-28 01:54:44 +0430

    Update translations.

--- src/translations/index.js
+++ src/translations/index.js
@@ -2,7 +2,10 @@ export default {
     EN: {
         PLACEHOLDER: 'Select an item',
         SEARCH_PLACEHOLDER: 'Type something...',
-        SELECTED_ITEMS_COUNT_TEXT: '{count} item(s) have been selected',
+        SELECTED_ITEMS_COUNT_TEXT: {
+            1: 'An item has been selected',
+            n: '{count} items have been selected'
+        },
         NOTHING_TO_SHOW: 'There\'s nothing to show!'
     },
     AR: {
@@ -14,13 +17,19 @@ export default {
     FA: {
         PLACEHOLDER: 'آیتمی انتخاب کنید',
         SEARCH_PLACEHOLDER: 'چیزی تایپ کنید...',
-        SELECTED_ITEMS_COUNT_TEXT: '{count} آیتم انتخاب شده است',
+        SELECTED_ITEMS_COUNT_TEXT: {
+            1: 'یک آیتم انتخاب شده است',
+            n: '{count} آیتم انتخاب شده است'
+        },
         NOTHING_TO_SHOW: 'چیزی برای نمایش وجود ندارد!'
     },
     TR: {
         PLACEHOLDER: 'Bir seçenek seçin',
         SEARCH_PLACEHOLDER: 'Arama...',
-        SELECTED_ITEMS_COUNT_TEXT: '{count} öğe seçildi',
+        SELECTED_ITEMS_COUNT_TEXT: {
+            1: 'Bir öğe seçildi',
+            n: '{count} öğe seçildi'
+        },
         NOTHING_TO_SHOW: 'Gösterecek hiçbir şey yok!'
     },
     RU: {
