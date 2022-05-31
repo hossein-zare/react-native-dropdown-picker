3ce3904306e6901dc3c74077d23da5bd39b110fc
mohamed <bourga.mohamed@gmail.com>
2022-05-31 12:07:27 +0100

    added a fix for picker crashing when multipe is enabled and an item is selected

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -1263,7 +1263,7 @@ function Picker({
 
         // Not a reliable method for external value changes.
         if (multiple) {
-            if (memoryRef.current.value.includes(item[_schema.value])) {
+            if (memoryRef.current.value?.includes(item[_schema.value])) {
                 const index = memoryRef.current.items.findIndex(x => x[_schema.value] === item[_schema.value]);
 
                 if (index > -1) {
@@ -1279,7 +1279,7 @@ function Picker({
 
         setValue(state => {
             if (multiple) {
-                let _state = state !== null ? [...state] : [];
+                let _state = state !== null && state !== undefined  ? [...state] : [];
 
                 if (_state.includes(item[_schema.value])) {
                     // Remove the value
